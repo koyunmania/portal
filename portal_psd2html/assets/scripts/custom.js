65bd733d9acd8b8d1206d5ce77adb45fa6185022
koyunmania <vv1357@hotmail.com>
2015-12-27 13:39:55 +0200

    adding all prtal group source code into git

---
+++ portal_psd2html/assets/scripts/custom.js
@@ -0,0 +1,26 @@
+$(function () {
+
+	// slider
+	var slider_items = $(".slider-item ul.slider-item-container > li");
+	var slider_buttons = $(".slider-button ul li");
+	
+	slider_items.filter(":not(:first)").hide();
+	slider_buttons.filter(":first").addClass("active");
+	slider_buttons.on("click", function (e) {
+		var index = $(this).index();
+		slider_items.fadeOut(500).filter(":eq(" + index + ")").fadeIn(500);
+		slider_buttons.removeClass("active");
+		$(this).addClass( "active" );
+		e.preventDefault();
+	});
+	
+	//product-list
+	$(".product-list ul li").hover( function () {
+		$(".title", this).finish().fadeOut(700);
+		$(".product-icon", this).finish().fadeIn(700);
+	}, function () {
+		$(".title", this).fadeIn(700);
+		$(".product-icon", this).fadeOut(700);
+	}
+	);
+});
